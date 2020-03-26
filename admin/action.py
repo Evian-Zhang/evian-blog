61f806e4dffd702d5b6e4f65d339457010d94801
Evian-Zhang <evianzhang1999@163.com>
2020-03-26 13:50:18 +0800

    add new manipulators

--- admin/action.py
+++ admin/action.py
@@ -1,5 +1,6 @@
 import toml
 import psycopg2
+import datetime
 
 def read_conf(config_path):
     with open(config_path, 'r') as config_file:
@@ -12,7 +13,7 @@ def establish_connection(config):
 
 def add_tag(cursor, tag_name):
     cursor.execute("""
-    INSERT INTO tags (name) VALUES (%s);
+    INSERT INTO tags (name, article_count) VALUES (%s, 0);
     """,
     (tag_name,)) # Python requires a comma to create a signle element tuple
 
@@ -34,37 +35,45 @@ def remove_series(cursor, series_name):
     """,
     (series_name,))
 
-def add_article(cursor, title, body, publish_date, tags, series=None):
-    try:
-        series_name, series_index = series
-        destructure_success = True
-    except:
-        destructure_success = False
-    if destructure_success:
-        cursor.execute("""
-        SELECT id FROM series WHERE name = %s;
-        """,
-        (series_name,))
-        series_id = cursor.fetchone()
-        if series_id == None:
-            raise Exception("Series name not found for " + series_name)
-        cursor.execute("""
-        INSERT INTO tables (title, body, publish_date, last_revise_date, series_id, series_index)
-        VALUES (%s, %s, %s, %s, %s, %s)
-        RETURNING id;
-        """,
-        (title, body, publish_date, publish_date, series_id, series_index))
-    else:
-        cursor.execute("""
-        INSERT INTO tables (title, body, publish_date, last_revise_date)
-        VALUES (%s, %s, %s, %s)
-        RETURNING id;
-        """,
-        (title, body, publish_date, publish_date))
-    article_id = cursor.fetchone()
-    
+def add_article_without_tag_and_series(cursor, title, body, publish_date):
+    cursor.execute("""
+    INSERT INTO articles (title, body, publish_date)
+    VALUES (%s, %s, %s)
+    RETURNING id;
+    """,
+    (title, body, publish_date))
+    return cursor.fetchone()    
 
-    
+def add_tags_of_article(cursor, article_id, tags):
+    # `ANY` can work with empty lists, wheras `IN()` is a SQL syntax error
+    cursor.execute("""
+    INSERT INTO tag_with_articles (tag_id, article_id)
+    SELECT tags.id, %s FROM tags
+    WHERE tags.name = ANY(%s);
+    """,
+    (article_id, tags))
+
+def remove_tag_of_article(cursor, article_title, tag_name):
+    cursor.execute("""
+    SELECT tags.id, articles.id
+    FROM tags
+    INNER JOIN articles
+    ON (tags.name = %s AND articles.title = %s)
+    """)
+    tag_id, article_id = cursor.fetchone()
+    cursor.execute("""
+    DELETE FROM tag_with_articles
+    WHERE (tag_id = %s AND article_id = %s);
+    """,
+    (tag_id, article_id))
+
+def add_series_of_article(cursor, article_id, series_id, series_index):
+    cursor.execute("""
+    UPDATE articles
+    SET series_id = %s, series_index = %s
+    WHERE id = %s;
+    """,
+    (series_id, series_index, article_id))
     
 
 # config = read_conf('config.toml')
@@ -72,6 +81,8 @@ def add_article(cursor, title, body, publish_date, tags, series=None):
 # try:
 #     with connection:
 #         with connection.cursor() as cursor:
-#             add_tag(cursor, 'mac')
+#             # add_tag(cursor, 'mac')
+#             # article_id = add_article_without_tag_and_series(cursor, "my-title", "my body", datetime.datetime.now())
+#             add_tags_of_article(cursor, 1, ['mac'])
 # finally:
 #     connection.close()
