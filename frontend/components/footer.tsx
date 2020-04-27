d544322747ec5bbb81ef4f4fd0c077863f4b009c
Evian-Zhang <evianzhang1999@163.com>
2020-04-27 11:42:29 +0800

    add more info to footer

--- frontend/components/footer.tsx
+++ frontend/components/footer.tsx
@@ -8,10 +8,10 @@ const MyFooter = () => {
             联系方式：
             <Space>
                 <div>
-                    GitHub<a href="https://github.com/Evian-Zhang" rel="noopener noreferrer"><GithubFilled/></a>
+                    GitHub <a href="https://github.com/Evian-Zhang" rel="noopener noreferrer"><GithubFilled/></a>
                 </div>
                 <div>
-                    Gmail<a href="mailto:evianzhang1999@gmail.com"><MailFilled/></a>
+                    Gmail <a href="mailto:evianzhang1999@gmail.com"><MailFilled/></a>
                 </div>
                 <div>
                     知乎 <a href="https://www.zhihu.com/people/Evian_Zhang" rel="noopener noreferrer">勥巭炛</a>
