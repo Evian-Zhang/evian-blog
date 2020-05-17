5fdb14516fbd11b9f0368323852a9367868cbd76
Evian-Zhang <evianzhang1999@163.com>
2020-05-17 10:39:52 +0800

    initial commits for iOS

---
+++ iOS/EvianBlog/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  EvianBlog
+//
+//  Created by Evian张 on 2020/5/17.
+//  Copyright © 2020 Evian张. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
