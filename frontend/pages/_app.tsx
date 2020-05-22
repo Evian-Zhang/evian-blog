e4ced3cbf358ed24b341a45f169a35da174fa7da
Evian-Zhang <evianzhang1999@163.com>
2020-05-22 10:16:29 +0800

    add performance matrice

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -2,6 +2,10 @@ import { AppProps } from 'next/app';
 
 import 'antd/dist/antd.css';
 
+export function reportWebVitals(metric) {
+    console.log(metric);
+};
+
 const MyApp = ({ Component, pageProps }: AppProps) => {
     return <Component {...pageProps} />
 };
