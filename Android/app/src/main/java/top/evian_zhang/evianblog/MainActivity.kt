c0481dfb33aec13f0f1ea515350ee4cbbd0aada2
Evian-Zhang <evianzhang1999@163.com>
2020-06-09 16:01:55 +0800

    add navigation views

--- Android/app/src/main/java/top/evian_zhang/evianblog/MainActivity.kt
+++ Android/app/src/main/java/top/evian_zhang/evianblog/MainActivity.kt
@@ -2,11 +2,48 @@ package top.evian_zhang.evianblog
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+import androidx.navigation.NavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private var currentNavController: LiveData<NavController>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            this.setupBottomNavigationBar()
+        }
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        setupBottomNavigationBar()
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return currentNavController?.value?.navigateUp() ?: false
+    }
+
+    // see https://github.com/android/architecture-components-samples/blob/master/NavigationAdvancedSample
+    private fun setupBottomNavigationBar() {
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        val navigationGraphIds = listOf(R.navigation.navigation_writings, R.navigation.navigation_projects, R.navigation.navigation_resume)
+
+        val controller = bottomNavigationView.setupWithNavController(
+            navGraphIds = navigationGraphIds,
+            fragmentManager = supportFragmentManager,
+            containerId = R.id.nav_host_fragment,
+            intent = intent
+        )
+
+        controller.observe(this, Observer { navController ->
+            setupActionBarWithNavController(navController)
+        })
+        currentNavController = controller
     }
 }
---
+++ Android/app/src/main/java/top/evian_zhang/evianblog/views/writings/WritingsFragment.kt
@@ -0,0 +1,23 @@
+package top.evian_zhang.evianblog.views.writings
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import top.evian_zhang.evianblog.R
+
+class WritingsFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_writings, container, false)
+    }
+}
